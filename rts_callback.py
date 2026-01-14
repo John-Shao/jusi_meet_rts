faedfddfef5627b80f8d25a006874231252d47d2
John-Shao <91682720+John-Shao@users.noreply.github.com>
2026-01-14 16:52:50 +0800

    删除冗余代码

--- rts_callback.py
+++ rts_callback.py
@@ -44,121 +44,6 @@ async def handle_rts_callback(request: Request, background_tasks: BackgroundTask
         status_code=200,
         content="ok",
     )
-'''
-# 处理用户加入房间事件
-async def handle_user_join_room(notify_msg: fication, event_data: Dict):
-    ujr_event = UserJoinRoomEvent(**event_data)
-    
-    if not ujr_event.UserId.startswith("jusi_"):
-        return  # 非jusi设备，不处理
-    
-    message = RequestMessageBase(
-        request_id=notify_msg.EventId,
-        app_id=notify_msg.AppId,
-        room_id=ujr_event.RoomId,
-        user_id=ujr_event.UserId,
-        event_name="vcJoinRoom",
-        device_id=ujr_event.UserId[5:] if ujr_event.UserId.startswith("jusi_") else ujr_event.UserId,
-        login_token="",
-    )
-
-    content = {
-        "user_name": ujr_event.UserId,
-        "camera": DeviceState.OPEN,
-        "mic": DeviceState.OPEN,
-        "is_silence": SilenceState.NOT_SILENT,
-    }
-
-    user_name = content.get("user_name")
-    camera = DeviceState(content.get("camera", DeviceState.CLOSED))
-    mic = DeviceState(content.get("mic", DeviceState.CLOSED))
-    is_silence = SilenceState(content.get("is_silence", SilenceState.NOT_SILENT))
-    
-    user = UserModel(
-        user_id=message.user_id,
-        device_id=message.device_id,
-        user_name=user_name,
-        camera=camera,
-        mic=mic,
-        is_silence=is_silence,
-    )
-
-    room: RoomModel = await service.join_room(message.app_id, user, message.room_id)
-
-    wb_room_id = f"whiteboard_{message.room_id}"
-    wb_user_id = f"whiteboard_{message.user_id}"
-    
-    response = JoinMeetingRoomRes(
-        room = room.model,
-        user= user.model,
-        user_list = room.get_user_list(),
-        token = generate_token(user._user.user_id, message.room_id),
-        wb_room_id = wb_room_id,
-        wb_user_id = wb_user_id,
-        wb_token = generate_token(wb_user_id, wb_room_id),
-    )
-
-    res = ResponseMessageBase(
-        request_id=message.request_id,
-        event_name=message.event_name,
-        response=response,
-    )
-
-    body = BroadcastMessageBase(
-        AppId=message.app_id,
-        RoomId=message.room_id,
-        Message=res.model_dump_json(),
-    )
-
-    req_json: str = body.model_dump_json()
-    logger.debug(f"准备发送广播消息: {req_json}")
-    
-    try:
-        response = rtc_service.send_broadcast(req_json)
-        logger.debug(f"返回消息: {json.dumps(response, indent=2, ensure_ascii=False)}")
-    except Exception as e:
-        logger.error(f"发送广播消息失败: {e}")
-
-# 处理用户离开房间事件
-async def handle_user_leave_room(notify_msg: CallbackNotification, event_data: Dict):
-    ulr_event = UserLeaveRoomEvent(**event_data)
-    
-    if not ujr_event.UserId.startswith("jusi_"):
-        return  # 非jusi设备，不处理
-
-    message = RequestMessageBase(
-        request_id=notify_msg.EventId,
-        app_id=notify_msg.AppId,
-        room_id=ulr_event.RoomId,
-        user_id=ulr_event.UserId,
-        event_name="vcLeaveRoom",
-        device_id=ulr_event.UserId[5:] if ulr_event.UserId.startswith("jusi_") else ulr_event.UserId,
-        login_token="",
-    )
-
-    await service.leave_room(message.user_id, message.room_id)
-
-    res = ResponseMessageBase(
-        request_id=message.request_id,
-        event_name=message.event_name,
-        response=None,
-    )
-
-    body = BroadcastMessageBase(
-        AppId=message.app_id,
-        RoomId=message.room_id,
-        Message=res.model_dump_json(),
-    )
-
-    req_json: str = body.model_dump_json()
-    logger.debug(f"准备发送广播消息: {req_json}")
-    
-    try:
-        response = rtc_service.send_broadcast(req_json)
-        logger.debug(f"返回消息: {json.dumps(response, indent=2, ensure_ascii=False)}")
-    except Exception as e:
-        logger.error(f"发送广播消息失败: {e}")
-    '''
 
 # 处理用户加入房间事件
 async def handle_user_join_room(notify_msg: RtsCallback, event_data: Dict):
@@ -256,6 +141,7 @@ async def handle_user_leave_room(notify_msg: RtsCallback, event_data: Dict):
         logger.debug(f"返回消息: {json.dumps(response, indent=2, ensure_ascii=False)}")
     except Exception as e:
         logger.error(f"发送广播消息失败: {e}")
+
 
 # 处理程序映射
 EVENT_HANDLERS = {
